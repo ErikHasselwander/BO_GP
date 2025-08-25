b7434158a3d624ca8b49be447313c31907c69783
ErikHasselwander <78478454+ErikHasselwander@users.noreply.github.com>
2025-08-25 10:13:00 +0200

    Update run_single_case.py

--- run_single_case.py
+++ run_single_case.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 
 baseGeometryDict = [
-      0.65, 0.4, 70, *([.25]*2), 0.1, *([.25]*4), 0.1, 0.95, 0.3, 0.8, 0.8, 3
+      0.65, 0.4, 70, *([.25]*2), 0.075, *([.25]*4), 0.075, 1.1, 0.3, 1, 0.85, 3
 ]
 
 a = StarManager()
 
-a.runSingleCase("meshconv", baseGeometryDict, Path.cwd() / "meshconvstudy", Path.cwd() / "refFiles")
+a.runSingleCase("case", baseGeometryDict, Path.cwd() / "cases", Path.cwd() / "refFiles")
